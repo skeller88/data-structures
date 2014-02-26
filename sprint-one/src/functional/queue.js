230a0baf9163386e7076eca6a9657500c4523603
Addison <Addisonedwardlee@gmail.com>
2014-02-26 14:38:26 -0800

    stack/queue complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,30 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
 
   instance.enqueue = function(value){
+    if(size > 0){
+      _.each(storage, function(item){
+        storage[item + 1] = storage[item]
+      })
+    }
+    storage[0] = value;
+    size++;
   };
 
   instance.dequeue = function(){
+    var item = storage[size-1];
+    if(size > 0){
+      delete storage[size-1];
+      size--;
+      return item;
+    }
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,24 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size = 0;
 
   // Implement the methods below
   instance.push = function(value){
+    storage[size++] = value;
   };
 
   instance.pop = function(){
+    var item = storage[size-1];
+    if(size > 0){
+      delete storage[size-1];
+      size--;
+      return item;
+    }
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
