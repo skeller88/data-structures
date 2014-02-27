a1acf3f2e09f6e87bb315b971b59504bada03488
skeller88 <skeller88@gmail.com>
2014-02-26 16:27:05 -0800

    Pass functional-shared queue and stack. Modify SpecRunner.html to display only prototypal specs.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,38 @@
-var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+/* global _ */
+/* exported makeQueue, queueMethods */
+var queueMethods = {
+  enqueue: function(value){
+    if(this.instanceSize > 0){
+      _.each(this.storage, function(item, key){
+
+        this.storage[Number(key)+1] = item;
+      }, this);
+    }
+    this.storage[0] = value;
+    this.instanceSize++;
+  },
+
+  dequeue: function(){
+    var item = this.storage[this.instanceSize-1];
+    if(this.instanceSize > 0){
+      delete this.storage[this.instanceSize-1];
+      this.instanceSize--;
+      return item;
+    }
+  },
+
+  size: function(){
+    return this.instanceSize;
+  }
 };
 
-var queueMethods = {};
+var makeQueue = function(){
+  var instance = {
+    instanceSize: 0,
+    storage: {}
+  };
+  _.extend(instance, queueMethods);
+  return instance;
+};
+
+
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,27 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+/* global _ */
+/* exported makeStack, stackMethods */
+var stackMethods = {
+  push : function(value){
+    this.storage[this.instanceSize++] = value;
+  },
+  pop : function(){
+    var item = this.storage[this.instanceSize-1];
+    if(this.instanceSize > 0){
+      delete this.storage[this.instanceSize-1];
+      this.instanceSize--;
+      return item;
+    }
+  },
+  size : function(){
+    return this.instanceSize;
+  }
 };
-  
-var stackMethods = {};
+
+var makeStack = function(){
+  var instance = {
+    instanceSize: 0,
+    storage: {}
+  };
+  _.extend(instance, stackMethods);
+  return instance;
+};
