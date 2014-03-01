ca4e241dca2a4bc91086f27630c0bbdc0163abff
skeller88 <skeller88@gmail.com>
2014-02-28 16:07:49 -0800

    binarySearchTree and specs

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,53 @@
 var makeBinarySearchTree = function(value){
+  var newBST = Object.create(bstMethods);
+  newBST.value = value;
+  newBST.left = undefined;
+  newBST.right = undefined;
+  return newBST;
+};
+
+var bstMethods = {
+  insert: function(value) {
+    var node = makeBinarySearchTree(value);
+    if (value > this.value) {
+      if (!this.right) {
+        this.right = node;
+      } else {
+        this.right.insert(value);
+      }
+    } else {
+      if (!this.left) {
+        this.left = node;
+      } else {
+        this.left.insert(value);
+      }
+    }
+  },
+
+  contains: function(value) {
+    if(this.value === value) {
+      return true;
+    }
+    if (this.left) {
+      if (this.left.contains(value)) {
+        return true;
+      }
+    }
+    if (this.right) {
+      if (this.right.contains(value)){
+        return true;
+      }
+    }
+    return false;
+  },
+
+  depthFirstLog: function(callback) {
+    callback(this.value);
+    if (this.left) {
+      this.left.depthFirstLog(callback);
+    }
+    if (this.right) {
+      this.right.depthFirstLog(callback);
+    }
+  }
 };
