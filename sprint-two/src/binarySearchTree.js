e79d061e8e960dd84bc4dfd9f694b7896703c958
skeller88 <skeller88@gmail.com>
2014-05-17 18:57:35 -0700

    Graph

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,9 @@
 makeBinarySearchTree = function(value){
     var node = {};
+    node.leftDepth = 1;
+    node.rightDepth = 1;
+    node.maxDepth = 1;
+    node.minDepth = 1;
     node.value = value || null;
 
     //assume new value won't have the same value as
@@ -7,18 +11,50 @@ makeBinarySearchTree = function(value){
     node.insert = function(value){
         if(value > node.value){
             if(node.right){
+                node.rightDepth++;
+                node.maxDepth = Math.max(node.rightDepth, node.leftDepth);
+                node.minDepth = Math.min(node.rightDepth, node.leftDepth);
                 node.right.insert(value);
+
+                if(node.minDepth && node.maxDepth/node.minDepth > 2){
+                    node.rebalance(); 
+                }
             }else{
                 node.right = makeBinarySearchTree(value);
             }
         }else if(value < node.value){
             if(node.left){
+                node.leftDepth++;
+                node.maxDepth = Math.max(node.rightDepth, node.leftDepth);
+                node.minDepth = Math.min(node.rightDepth, node.leftDepth);
                 node.left.insert(value);
+
+                if(node.minDepth && node.maxDepth/node.minDepth > 2){
+                    node.rebalance(); 
+                }
             }else{
                 node.left = makeBinarySearchTree(value);
             }
         }else{
             throw 'A node with this value already exists.'
+        }
+    };
+
+    node.rebalance = function(){
+        var orderedNodes = [];
+        node.leftDepth = 0;
+        node.rightDepth = 0;
+        
+        node.depthFirstLog(function(value){
+            orderedNodes.push(value);
+        });
+
+        var mid = orderedNodes.splice(orderedNodes.length/2, 1)[0];
+
+        node = makeBinarySearchTree(mid);
+
+        for (var i = 0; i < orderedNodes.length; i++) {
+            node.insert(orderedNodes[i]);
         }
     };
 
@@ -52,14 +88,14 @@ makeBinarySearchTree = function(value){
         var toCheck = [node];
         var result = [];
 
-        while(node = toCheck.shift()){
-            result.push(node.value);
+        while(nextNode = toCheck.shift()){
+            result.push(nextNode.value);
             
-            if(node.left){
-                toCheck.push(node.left);
+            if(nextNode.left){
+                toCheck.push(nextNode.left);
             }
-            if(node.right){
-                toCheck.push(node.right);
+            if(nextNode.right){
+                toCheck.push(nextNode.right);
             }
         }
         return result;
@@ -67,3 +103,12 @@ makeBinarySearchTree = function(value){
 
     return node;
 };
+
+var n = makeBinarySearchTree(4);
+n.insert(6);
+n.breadthFirstLog();
+n.breadthFirstLog();
+n.insert(7);
+n.insert(8);
+n.insert(9);
+n.breadthFirstLog();
--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,20 +1,76 @@
 var Graph = function(){
+    this.nodes = {};
+    this.edges = {};
+    this.orderAdded = [];
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+    if(!newNode) return;
+
+    this.nodes[newNode] = true;
+    this.orderAdded.push(newNode);
+    this.edges[newNode] = {};
+
+    if(this.orderAdded.length === 1){
+        this.firstNode = newNode;
+    }else if(this.orderAdded.length === 2 && !toNode){
+        toNode = this.firstNode;
+    }
+
+    if(this.orderAdded.length >= 2){
+        this.edges[newNode][toNode] = true;
+        this.edges[toNode][newNode] = true;
+    }
 };
 
 Graph.prototype.contains = function(node){
+    return !!this.nodes[node];
 };
 
 Graph.prototype.removeNode = function(node){
+    delete this.nodes[node];
+
+    if(node === this.firstNode){
+        this.orderAdded.shift();
+
+        if(this.orderAdded.length){
+            this.firstNode = this.orderAdded[0];
+        }
+    }
+
+    var deletedEdges = Object.keys(this.edges[node]);
+    delete this.edges[node];
+
+    for (var i = 0; i < deletedEdges.length; i++) {
+        //'connected node'
+        var cn = deletedEdges[i];
+         delete this.edges[cn][node];
+         if(!Object.keys(cn).length){
+             this.removeNode(cn);
+         }
+     }
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
+    return !!this.edges[fromNode][toNode];
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+    this.edges[fromNode][toNode] = true;
+    this.edges[toNode][fromNode] = true;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+    delete this.edges[fromNode][toNode];
+    delete this.edges[toNode][fromNode];
+    if(!Object.keys(this.edges[fromNode]).length){
+        this.removeNode(fromNode);
+    }
+    if(!Object.keys(this.edges[toNode]).length){
+        this.removeNode(toNode);
+    }
 };
+
+var g = new Graph();
+g.addNode(1);
+// g.addNode(2)
